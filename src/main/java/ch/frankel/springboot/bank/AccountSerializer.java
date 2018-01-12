3e08ac1db91af6edc5f3aa84544efeb53e6848c4
Nicolas Fränkel <nicolas@frankel.ch>
2018-01-12 19:21:03 +0100

    Flatten returned JSON structure

---
+++ src/main/java/ch/frankel/springboot/bank/AccountSerializer.java
@@ -0,0 +1,21 @@
+package ch.frankel.springboot.bank;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+@JsonComponent
+public class AccountSerializer extends JsonSerializer<Account> {
+
+    @Override
+    public void serialize(Account account, JsonGenerator generator, SerializerProvider serializerProvider) throws IOException {
+        generator.writeStartObject();
+        generator.writeNumberField("id", account.getId());
+        generator.writeNumberField("amount", account.getAmount());
+        generator.writeStringField("iban", account.getIban().getNumber());
+        generator.writeEndObject();
+    }
+}
